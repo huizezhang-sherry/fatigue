333758eb360650c9cef3d7b32a3b35a2a26fe9d9
Huizezhangsherry <36781502+huizezhang-sherry@users.noreply.github.com>
2018-11-22 21:03:44 +1100

    Extract all the record for a particular player for a match

---
+++ R/ExtractRecordall.R
@@ -0,0 +1,34 @@
+
+extract_records_all <- function(dt1, dt2, name){
+  # read in the data 
+  match <- read_csv(dt1)
+  point <- read_csv(dt2)
+  data <- left_join(match, point) %>% 
+    mutate(Gender = as.factor(ifelse(test = match_num <2000,yes = 'Male',no = 'Female')))
+  dt1 <- data[data$player1 == name,]
+  dt2 <- data[data$player2 == name,]
+  dt <- rbind(dt1,dt2)
+  
+  ## tidying data
+  dt<- dt[!is.na(dt$match_id),]
+  # Re-arrange dt based on match_num
+  
+  dt <- dt %>% arrange(match_num) 
+  
+  # Create new variables
+  dt <- dt %>% 
+    mutate(point = seq(1,length(dt$SetNo),1)) %>% 
+    mutate(game_ = as.integer(ifelse(dt$GameWinner == 0,0,1))) %>% 
+    mutate(round = as.factor((dt$match_num - 1000) %/% 100)) %>% 
+    mutate(match_num = as.factor(dt$match_num)) %>% 
+    mutate(round = as.numeric(substr(match_id, 15, 15))) %>% 
+    mutate(set_ = as.integer(ifelse(dt$SetWinner ==0, 0, 1)))
+  
+  dt <- dt %>% 
+    mutate(game = as.integer(cumsum(dt$game_)+1))
+  
+  return(dt)
+}
+
+Federer = extract_records_all("data/2017-ausopen-matches.csv", "data/2017-ausopen-points.csv", "Roger Federer")
+Nadal = extract_records_all("data/2017-ausopen-matches.csv", "data/2017-ausopen-points.csv", "Rafael Nadal")
