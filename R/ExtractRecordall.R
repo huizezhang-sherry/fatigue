8e6a7fe97e64632ec1f5a6b82c51f99e24f5d505
Huizezhangsherry <36781502+huizezhang-sherry@users.noreply.github.com>
2018-12-12 14:45:28 +1100

    add more variables

--- R/ExtractRecordall.R
+++ R/ExtractRecordall.R
@@ -18,22 +18,21 @@ extract_records_all <- function(dt1, dt2, name){
   dt <- dt %>% arrange(match_num) 
   
   # Create new variables
-  dt <- dt %>% 
-    #mutate(index_point = seq(1,length(dt$SetNo),1)) %>% 
-    mutate(game_ = as.integer(ifelse(dt$GameWinner == 0,0,1))) %>% 
-    mutate(round = as.factor((dt$match_num - 1000) %/% 100)) %>% 
-    mutate(match_num = as.factor(dt$match_num)) %>% 
-    mutate(round = as.numeric(substr(match_id, 15, 15))) %>% 
-    mutate(set_ = as.integer(ifelse(dt$SetWinner ==0, 0, 1))) %>% 
-    mutate(index_set = as.numeric(paste(match_num,SetNo, sep = ""))) %>% 
-    mutate(dist= ifelse(player1 == name, P1DistanceRun, P2DistanceRun)) %>% 
-    mutate(index_game = (paste(match_num,SetNo,GameNo, sep = ""))) %>% 
-    mutate(MatchNo = as.numeric(substr(dt$match_num,2,2)))
-  
+  dt <- dt %>%
+    #mutate(index_point = seq(1,length(dt$SetNo),1)) %>%
+    mutate(game_ = as.integer(ifelse(dt$GameWinner == 0,0,1))) %>%
+    mutate(round = as.factor((dt$match_num - 1000) %/% 100)) %>%
+    mutate(match_num = as.factor(dt$match_num)) %>%
+    mutate(round = as.numeric(substr(match_id, 15, 15))) %>%
+    mutate(set_ = as.integer(ifelse(dt$SetWinner ==0, 0, 1))) %>%
+    mutate(index_set = as.numeric(paste(match_num,SetNo, sep = ""))) %>%
+    mutate(dist= ifelse(player1 == name, P1DistanceRun, P2DistanceRun)) %>%
+    mutate(index_game = (paste(match_num,SetNo,GameNo, sep = ""))) %>%
+    mutate(MatchNo = as.factor(substr(dt$match_num,2,2))) %>%
+    mutate(time = c(0,as.vector(diff(dt$ElapsedTime)))) %>%
+    mutate(cum_time = cumsum(time)/60)
+
   dt <- dt %>% filter(PointNumber != 0)
   return(dt)
 }
 
-# Federer = extract_records_all("../data/2017-ausopen-matches.csv", "../data/2017-ausopen-points.csv", "Roger Federer")
-# Nadal = extract_records_all("../data/2017-ausopen-matches.csv", "../data/2017-ausopen-points.csv", "Rafael Nadal")
-# Alex =extract_records_all("../data/2017-ausopen-matches.csv", "../data/2017-ausopen-points.csv", "Alexander Zverev")
